664f006d1ac2be260203d8fc04f1c94d7e9d144f
Andy Maskiell <maskiella@gmail.com>
2025-01-25 20:32:45 -0500

    Some more linting and formating fixes :B

---
+++ app/webpack.config.js
@@ -0,0 +1,53 @@
+import path from 'path';
+
+const isProduction = (envVar) => envVar == 'production';
+
+const __dirname = path.resolve();
+
+// const babelOptions = {
+//     presets: ['@babel/preset-env', '@babel/preset-react'],
+// };
+
+const buildConfig = (env, argv) => ({
+  context: path.resolve(__dirname),
+  devtool: 'inline-source-map',
+  entry: {
+    consumers: './src/consumers/index.ts',
+    producers: './src/producers/index.ts'
+  },
+  output: {
+    filename: '[name]-[chunkhash].min.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          // {
+          //   loader: 'babel-loader',
+          //   options: babelOptions,
+          // },
+          {
+            loader: 'ts-loader',
+            // options: {
+            //   compilerOptions: {
+            //     noEmit: false,
+            //   },
+            // },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  }
+})
+
+export default (env, argv) => {
+  const config = buildConfig(env, argv);
+
+  return config;
+};
