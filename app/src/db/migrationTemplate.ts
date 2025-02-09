c215f23aca01d7a6656f3f63db13904a838f9cd5
Andy Maskiell <maskiella@gmail.com>
2025-02-09 18:18:27 -0500

    Add initial setup for Knex and PostgreSQL, fix a bunch of scripts

---
+++ app/src/db/migrationTemplate.ts
@@ -0,0 +1,9 @@
+import type { Knex } from 'knex';
+
+export const up = (knex: Knex) => {
+  // TODO: implement me! :D
+};
+
+export const down = (knex: Knex) => {
+  // TODO: implement me! :D
+};
---
+++ app/src/db/postgres-client/index.ts
@@ -0,0 +1,15 @@
+import knex from 'knex';
+
+const postgresClient = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.PG_HOST,
+    port: Number(process.env.PG_PORT),
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE_NAME,
+    // ssl: process.env.POSTGRES_SSL ? { rejectUnauthorized: false }: false
+  },
+});
+
+export { postgresClient };
