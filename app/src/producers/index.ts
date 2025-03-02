94dbb6d66f330d543bd8a727edca415aac85e8c6
Andy Maskiell <maskiella@gmail.com>
2025-03-02 14:27:47 -0500

    Refactor and clean up producers

--- app/src/producers/index.ts
+++ app/src/producers/index.ts
@@ -1,52 +1,33 @@
 import util from 'node:util';
 import { Producer } from 'kafkajs';
-import { faker } from '@faker-js/faker';
 
-import { topicSchemas, type PersonTopicRecord } from '@/_schemas/register-schemas';
+import { topicSchemas } from '@/_schemas/register-schemas';
 import { getMiniAppKafkaProducer } from '@/common/kafka-clients';
 import { Services } from '@/constants';
-import { fullW, halfW, loggers, spacer } from '@/utils';
+import {
+  fullW,
+  generateRandomPerson,
+  halfW,
+  loggers,
+  logInfoWithNewlines,
+  randomDelay,
+  spacer,
+} from '@/utils';
 
 const producersLogger = loggers.get(Services.PRODUCERS);
 
 const personTopicSchema = topicSchemas[0];
 let producer: Producer = null;
 
-// TODO: Move this to utils
-function logInfoWithNewlines(message: string) {
-  producersLogger.info('\n');
-  producersLogger.info(message);
-  producersLogger.info('\n');
-}
-
-function generateRandomPerson(): PersonTopicRecord {
-  const birthDateAsNegativeInt = faker.date.birthdate().getTime();
-  return {
-    firstName: faker.person.firstName(),
-    lastName: faker.person.lastName(),
-    age: faker.number.int({ min: 1, max: 100 }),
-    birthDate: birthDateAsNegativeInt * -1,
-  };
-}
-
-const between0And5SecondsInMilli = () => (Math.random() * 5) * 1000;
-
-async function randomDelay() {
-  return new Promise((resolve) => {
-    const delayMs = between0And5SecondsInMilli();
-
-    logInfoWithNewlines(`${spacer}Random delay of ${delayMs / 1000} seconds...`);
-
-    setTimeout(() => {
-      resolve(true);
-    }, delayMs);
-  });
-}
+// TODO: this is a little hacky :]
+const localLogInfo = (message: string) => {
+  logInfoWithNewlines(message, producersLogger);
+};
 
 async function intermittentlyProduceMessages() {
-  logInfoWithNewlines(`    begin - intermittentlyProduceMessages @ ${Date.now()}`.padStart(halfW, '-'));
+  localLogInfo(`    begin - intermittentlyProduceMessages @ ${Date.now()}`.padStart(halfW, '-'));
   const recordKey = global.crypto.randomUUID();
-  logInfoWithNewlines(`${spacer}recordKey: ${recordKey}`);
+  localLogInfo(`${spacer}recordKey: ${recordKey}`);
   const personRecord = generateRandomPerson();
   producersLogger.info(util.inspect(personRecord, { colors: true, depth: null }));
 
@@ -55,12 +36,12 @@ async function intermittentlyProduceMessages() {
     value: JSON.stringify(personRecord),
   };
 
-  logInfoWithNewlines(`    Producing message for key '${recordKey}'...`.padStart(halfW, '?'));
+  localLogInfo(`    Producing message for key '${recordKey}'...`.padStart(halfW, '?'));
   await producer.send({
     topic: personTopicSchema.topicName,
     messages: [outgoingMessage],
   });
-  logInfoWithNewlines(`    Success! Produced message for key '${recordKey}' :D`.padStart(halfW, '!'));
+  localLogInfo(`    Success! Produced message for key '${recordKey}' :D`.padStart(halfW, '!'));
 
   await randomDelay();
 
@@ -70,8 +51,8 @@ async function intermittentlyProduceMessages() {
 async function producersMain() {
   producer = await getMiniAppKafkaProducer();
 
-  logInfoWithNewlines('    producers - main    '.padStart(halfW, '=').padEnd(fullW, '='));
-  logInfoWithNewlines(`${spacer}Starting producers...`);
+  localLogInfo('    producers - main    '.padStart(halfW, '=').padEnd(fullW, '='));
+  localLogInfo(`${spacer}Starting producers...`);
 
   await producer.connect().catch((e) => {
     producersLogger.error('    ERROR opening producer connection    '.padStart(halfW, '!').padEnd(fullW, '!'));
@@ -89,7 +70,7 @@ async function producersMain() {
     }
   }
 
-  logInfoWithNewlines(`    Producers shutting down...`.padStart(halfW, '=').padEnd(fullW, '='));
+  localLogInfo(`    Producers shutting down...`.padStart(halfW, '=').padEnd(fullW, '='));
 };
 
 producersMain().catch(async (e) => {
